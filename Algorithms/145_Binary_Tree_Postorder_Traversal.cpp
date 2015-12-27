ea7dc71efab9e72dad03921752c63b0dab11c834
Timer <linyicx@126.com>
2015-12-27 16:06:14 +0800

    two tree traversal

---
+++ Algorithms/145_Binary_Tree_Postorder_Traversal.cpp
@@ -0,0 +1,36 @@
+/*
+Time: O(n)
+Space: O(1)
+Runtime: 0ms
+
+by Timer [linyicx@126.com]
+2015.12.26
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution
+{
+private:
+    void traversal(vector<int>& ret, TreeNode* x)
+    {
+        if (x->left) traversal(ret,x->left);
+        if (x->right) traversal(ret,x->right);
+        ret.push_back(x->val);
+    }
+    
+public:
+    vector<int> postorderTraversal(TreeNode* root)
+    {
+        vector<int> ret;
+        ret.clear();
+        if (root) traversal(ret,root);
+        return ret;
+    }
+};
